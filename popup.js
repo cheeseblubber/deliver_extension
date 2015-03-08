858a3dd8067b21717344bcb598a64308f65f9c7e
Kam Leung <Kamleung911@gmail.com>
2015-03-07 22:44:33 -0800

    clicks hidden cash button

---
+++ popup.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Global variable containing the query we'd like to pass to Flickr. In this
+ * case, kittens!
+ *
+ * @type {string}
+ */
+
+console.log("IM IN HERE")
+// document.getElementsByClassName('click-cash')[0].onclick = function clickCash() {
+  // console.log("CMON GUYS");
+setTimeout(function(){
+  document.getElementsByClassName('select-payment-type')[0].children[2].click()
+}, 1000 );
